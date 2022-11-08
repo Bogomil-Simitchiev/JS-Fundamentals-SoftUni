ce0fd7e2fc49e889ec0babdecfe47fb0d47288a1
Bogomil Simitchiev <simitchiev365@abv.bg>
2022-11-08 23:49:30 +0200

    Added 2 exercises

---
+++ Data Types and Variables - Exercises/ConvertMetersToKilometres.js
@@ -0,0 +1,5 @@
+function MetersToKilometres(metres){
+
+    console.log(Number(metres/1000).toFixed(2));
+}
+MetersToKilometres(798)
---
+++ Data Types and Variables - Exercises/PoundsToDollars.js
@@ -0,0 +1,5 @@
+function PoundsToDollars(pounds){
+
+    console.log(Number(pounds*1.31).toFixed(3));
+}
+PoundsToDollars(80)
