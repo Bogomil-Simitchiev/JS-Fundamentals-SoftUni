6096bf02a7d381904d9e54088ae3ad663575609c
Bogomil Simitchiev <simitchiev365@abv.bg>
2022-11-09 00:06:55 +0200

    Added new exercises

---
+++ Data Types and Variables - Exercises/LowerOrUpper.js
@@ -0,0 +1,10 @@
+function Check(character){
+
+    if (character===character.toUpperCase()) {
+        console.log("upper-case")
+    }
+    else{
+        console.log("lower-case")
+    }
+}
+Check('T');
---
+++ Data Types and Variables - Exercises/ReversedChars.js
@@ -0,0 +1,9 @@
+function reverseCharacters(first, second, third) {
+
+    let result = `${third} ${second} ${first}`;
+    console.log(result);
+}
+reverseCharacters("A",
+    "B",
+    "C"
+)
